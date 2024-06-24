2a5f802ac66e36bc2db853590d152efa048e6095
eyedeo <143570721+eyedeo@users.noreply.github.com>
2024-06-24 22:06:34 +0000

    Added another button

--- js/main.js
+++ js/main.js
@@ -2,4 +2,7 @@ $( document ).ready(function() {
     $('#btn').click(function() {
         window.alert("pong");
     });
+    $('#btn2').click(function() {
+        window.alert("ping");
+    });
 });
